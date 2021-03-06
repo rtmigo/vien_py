ab447e3514e2cadf5731d419b2d4ab48ae108e10
rtmigo <art.galkin@gmail.com>
2021-03-06 04:14:43 +0300

    refactor

--- test.py
+++ test.py
@@ -2,10 +2,12 @@
 
 if __name__ == "__main__":
     import unittest, os
+
     parent_dir = os.path.dirname(__file__)
+
     tests = unittest.TestLoader().discover(
         top_level_dir=parent_dir,
         start_dir=os.path.join(parent_dir, "svet"),
         pattern="*.py")
-    testRunner = unittest.runner.TextTestRunner(buffer=True)
-    testRunner.run(tests)
+
+    unittest.runner.TextTestRunner(buffer=True).run(tests)
