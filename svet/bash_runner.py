494a07be6181e81718d2d302054c18d9e5be91b1
rtmigo <art.galkin@gmail.com>
2021-03-06 19:52:54 +0300

    run_in_bash_script

--- svet/bash_runner.py
+++ svet/bash_runner.py
@@ -2,18 +2,13 @@ import subprocess
 from typing import *
 
 
-def run_as_bash_script(commands: Union[List[str], str]):
-    bashLines = [
-        "#!/bin/bash"
-        # "set -e",  # fail on first error
-    ]
-
-    if isinstance(commands, str):
-        commands = [commands]
-
-    bashLines.extend(commands)
-
+def run_as_bash_script(command: str) -> subprocess.CompletedProcess:
     # we need executable='/bin/bash' for Ubuntu, it will run /bin/sh otherwise.
     # In MacOS 10.13 it seems to be optional
-    return subprocess.call("\n".join(bashLines), shell=True, executable='/bin/bash')
+    #return subprocess.call(command, shell=True, executable='/bin/bash')
+    return subprocess.run(command, shell=True, executable='/bin/bash')
+
+
+#def run_as_bash_script_and_communicate(command: str):
+ #   return subprocess.(command, shell=True, executable='/bin/bash')
 
--- svet/bash_runner_test.py
+++ svet/bash_runner_test.py
@@ -11,27 +11,27 @@ class TestRunAsBash(unittest.TestCase):
         bash_lines = [
             f'set -e',
             f"ls"]
-        code = run_as_bash_script(bash_lines)
-        self.assertEqual(code, 0)  # ok
+        code = run_as_bash_script("\n".join(bash_lines))
+        self.assertEqual(code.returncode, 0)  # ok
 
     def test_bad_command_error_code(self):
         bash_lines = [
             f'set -e',
             f"ok_computer_make_me_happy"]
-        code = run_as_bash_script(bash_lines)
-        self.assertEqual(code, 127)  # error
+        code = run_as_bash_script("\n".join(bash_lines))
+        self.assertEqual(code.returncode, 127)  # error
 
     def test_alias_expansion(self):
         with TemporaryDirectory() as td:
             file_to_create = Path(td) / "to_be_or_not_to_be.txt"
             file_to_create_quoted = repr(str(file_to_create))
-            bashLines = [
+            bash_lines = [
                 f'set -e',
                 f"shopt -s expand_aliases",
                 f'alias ohoho="echo"',  # this will work in bash, but not in sh
                 f'ohoho "that is the answer" > {file_to_create_quoted}']
             self.assertFalse(file_to_create.exists())
-            code = run_as_bash_script(bashLines)
-            self.assertEqual(code, 0)
+            code = run_as_bash_script("\n".join(bash_lines))
+            self.assertEqual(code.returncode, 0)
             self.assertTrue(file_to_create.exists())
             self.assertEqual(file_to_create.read_text().strip(), "that is the answer")
