a29c5db38a2b5993d4922451c3bafafa782e6dc7
rtmigo <art.galkin@gmail.com>
2021-03-06 04:17:47 +0300

    formatting

--- setup.py
+++ setup.py
@@ -7,60 +7,60 @@ constants = SourceFileLoader('constants', 'svet/constants.py').load_module()
 
 
 def readDocText():
-  """Reads README.md and returns the text after first empty line.
-  It helps to skip the header with badges intended only for GitHub.
-  """
-  doctext = (Path(__file__).parent / 'README.md').read_text()
+    """Reads README.md and returns the text after first empty line.
+    It helps to skip the header with badges intended only for GitHub.
+    """
+    doctext = (Path(__file__).parent / 'README.md').read_text()
 
-  # skipping until first empty line
-  lines = [l.strip() for l in doctext.splitlines()]
-  firstEmptyLineIndex = lines.index("")
-  lines = lines[firstEmptyLineIndex + 1:]
+    # skipping until first empty line
+    lines = [l.strip() for l in doctext.splitlines()]
+    firstEmptyLineIndex = lines.index("")
+    lines = lines[firstEmptyLineIndex + 1:]
 
-  return "\n".join(lines)
+    return "\n".join(lines)
 
 
 setup(
-  name="svet",
-  version=constants.__version__,
+    name="svet",
+    version=constants.__version__,
 
-  author="Art Galkin",
-  author_email="ortemeo@gmail.com",
-  url='https://github.com/rtmigo/svet',
+    author="Art Galkin",
+    author_email="ortemeo@gmail.com",
+    url='https://github.com/rtmigo/svet',
 
-  packages=find_packages(),
-  install_requires=[],
+    packages=find_packages(),
+    install_requires=[],
 
-  description="Command-line shortcuts for Python virtual environments",
+    description="Command-line shortcuts for Python virtual environments",
 
-  long_description=readDocText(),
-  long_description_content_type='text/markdown',
+    long_description=readDocText(),
+    long_description_content_type='text/markdown',
 
-  license='BSD-3-Clause',
+    license='BSD-3-Clause',
 
-  entry_points={
-    'console_scripts': [
-      'svet = svet:main_entry_point',
-    ]},
+    entry_points={
+        'console_scripts': [
+            'svet = svet:main_entry_point',
+        ]},
 
-  keywords="virtual-environment venv virtualenv python".split(),
+    keywords="virtual-environment venv virtualenv python".split(),
 
-  # https://pypi.org/classifiers/
-  classifiers=[
-    # "Development Status :: 4 - Beta",
-    # "Development Status :: 2 - Pre-Alpha",
-    "Development Status :: 3 - Alpha",
-    "Intended Audience :: Developers",
-    'License :: OSI Approved :: BSD License',
-    "Programming Language :: Python :: 3.7",
-    "Programming Language :: Python :: 3.8",
-    "Programming Language :: Python :: 3.9",
-    "Typing :: Typed",
-    "Topic :: Software Development :: Build Tools",
-    "Operating System :: POSIX"
-  ],
+    # https://pypi.org/classifiers/
+    classifiers=[
+        # "Development Status :: 4 - Beta",
+        # "Development Status :: 2 - Pre-Alpha",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        'License :: OSI Approved :: BSD License',
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Typing :: Typed",
+        "Topic :: Software Development :: Build Tools",
+        "Operating System :: POSIX"
+    ],
 
-  # test_suite='nose.collector',
-  # tests_require=['nose'],
-  # zip_safe=False
+    # test_suite='nose.collector',
+    # tests_require=['nose'],
+    # zip_safe=False
 )
