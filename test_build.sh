52b14b8e5fc611bd1b3ca200b75bf4ab71aeebb2
rtmigo <art.galkin@gmail.com>
2021-03-07 19:32:19 +0300

    testing for the package building

--- test_build.sh
+++ test_build.sh
@@ -5,4 +5,7 @@ scriptParentDir="$(dirname "$(perl -MCwd -e 'print Cwd::abs_path shift' "$0")")"
 cd "$scriptParentDir"
 
 python3 setup.py sdist bdist_wheel
-rm -rf ./build ./dist
+twine check ./dist/* --strict
+
+# tear down
+rm -rf ./build ./dist ./*.egg-info
