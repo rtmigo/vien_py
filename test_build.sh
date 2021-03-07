d572ff8cc37c9f47312ac9a18a91c225573c98f8
rtmigo <art.galkin@gmail.com>
2021-03-07 19:49:50 +0300

    comments

--- test_build.sh
+++ test_build.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 set -e
 
+## SET UP ####
+
 scriptParentDir="$(dirname "$(perl -MCwd -e 'print Cwd::abs_path shift' "$0")")"
 cd "$scriptParentDir"
+
+## TEST ####
 
 python3 setup.py sdist bdist_wheel
 twine check ./dist/* --strict
 
-# tear down
+## TEAR DOWN ####
+
 rm -rf ./build ./dist ./*.egg-info
