53d92e8d5ab2679258eb53d9ac0d448bccba6722
rtmigo <art.galkin@gmail.com>
2021-05-20 03:51:15 +0300

    fmt

--- vien/bash_runner.py
+++ vien/bash_runner.py
@@ -27,11 +27,12 @@ def _run_with_input_delay(*popenargs, input_delay: float = None,
                           check: bool = False,
                           capture_output: bool = False,
                           **kwargs):
-    """Basically the same as subprocess.run, but accepts input_delay parameter."""
+    """Basically the same as subprocess.run, but accepts input_delay
+    parameter."""
 
     # This is almost an exact copy of subprocess.run (as of 2021-03-06).
-    # SPDX-FileCopyrightText: (c) 2003-2005 Peter Astrand <astrand@lysator.liu.se>
-    # Latest version here: https://github.com/python/cpython/blob/master/Lib/subprocess.py
+    # SPDX-FileCopyrightText: 2003-2005 Peter Astrand <astrand@lysator.liu.se>
+    # https://github.com/python/cpython/blob/master/Lib/subprocess.py
 
     # START of insert from another portion of subprocess.py
     try:
