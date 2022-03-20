2916f06fbf645c9ae2805aa06fd955b7765705da
Artem IG <ortemeo@gmail.com>
2022-03-20 03:17:29 +0300

    upd

--- vien/_constants.py
+++ vien/_constants.py
@@ -1,3 +1,3 @@
-__version__ = "8.1.2"
-__copyright__ = "(c) 2020-2022 Artëm IG <github.com/rtmigo>"
+__version__ = "8.1.2+1"
+__copyright__ = "(c) 2020-2022 Artem IG <github.com/rtmigo>"
 __license__ = "BSD-3-Clause"
